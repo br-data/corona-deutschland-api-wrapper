3e1eb637d716cb99718701f509d8c40a86fa6cac
Steffen Kühne <stekhn@gmail.com>
2020-03-30 23:03:43 +0200

    Add JSON parsing, update URL

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fetch = require('node-fetch');
+const jsonToCsv = require('./lib/json-to-csv')
 
-const url ='https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=Meldedatum%3Etimestamp%20%272020-03-01%2022%3A59%3A59%27&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=ObjectId%2CAnzahlFall%2CMeldedatum%2CNeuerFall&orderByFields=Meldedatum%20asc&resultOffset=14000&resultRecordCount=2000&cacheHint=true';
+const url ='https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/Historic_adm0_v3/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=OBJECTID%2CNewCase%2CDateOfDataEntry&orderByFields=DateOfDataEntry%20asc&resultOffset=0&resultRecordCount=2000&cacheHint=true';
 
 (async function init() {
-  const reportedCases = await scrapeBody(url);
-
-  console.log(reportedCases);
+  const dataJson = await getJson(url);
+  const reportedCases = dataJson.features;
+  console.log(reportedCases.length);
 })();
 
-async function scrapeBody(url) {
+async function getJson(url) {
   return fetch(url)
-    .then(res => res.text())
+    .then(res => res.json())
     .catch(console.error);
 }
